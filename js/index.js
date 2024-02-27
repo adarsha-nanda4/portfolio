902e8032e6827828b8b78ee91e10ef38db68b63e
Adarsh Nanda <adarshananda51@gmail.com>
2024-02-27 16:03:42 +0530

    js

--- js/index.js
+++ js/index.js
@@ -19,9 +19,30 @@ backbtn.addEventListener("click",function(){
     
 
 });
+
+// -----------------------------------link projexte
+
 let pixel=document.getElementById("pixel");
+let face=document.getElementById("face");
+let study=document.getElementById("study");
+let cutmbol=cutmbol.getElementById("cutmbol");
+
+
+
 pixel.addEventListener("click",function(){
     window.location=("https://github.com/adarsha-nanda4/pixel-website")
+});
+
+face.addEventListener("click",function(){
+    window.location=("https://github.com/adarsha-nanda4/Face-Attendance")
+});
+
+study.addEventListener("click",function(){
+    window.location=("https://github.com/adarsha-nanda4/Study-Sync")
+});
+
+cutmbol.addEventListener("click",function(){
+    window.location=("https://github.com/adarsha-nanda4/CUTMBOL-Website")
 })
 
 
