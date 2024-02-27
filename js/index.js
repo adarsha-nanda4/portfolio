7718ae456bba716b0f1c18babb65b53afe964233
Adarsh Nanda <adarshananda51@gmail.com>
2024-02-27 16:17:05 +0530

    add js

--- js/index.js
+++ js/index.js
@@ -20,30 +20,31 @@ backbtn.addEventListener("click",function(){
 
 });
 
-// -----------------------------------link projexte
-
 let pixel=document.getElementById("pixel");
 let face=document.getElementById("face");
 let study=document.getElementById("study");
-let cutmbol=cutmbol.getElementById("cutmbol");
+let cutmbol=document.getElementById("cutmbol");
 
-
-
-pixel.addEventListener("click",function(){
-    window.location=("https://github.com/adarsha-nanda4/pixel-website")
-});
 
 face.addEventListener("click",function(){
     window.location=("https://github.com/adarsha-nanda4/Face-Attendance")
-});
+})
+
+
+pixel.addEventListener("click",function(){
+    window.location=("https://github.com/adarsha-nanda4/Pixel-Wallpaper-Website")
+})
 
 study.addEventListener("click",function(){
     window.location=("https://github.com/adarsha-nanda4/Study-Sync")
 });
 
-cutmbol.addEventListener("click",function(){
-    window.location=("https://github.com/adarsha-nanda4/CUTMBOL-Website")
+study.addEventListener("click",function(){
+    window.location=("https://www.cutmbol.com/")
 })
+
+
+
 
 
 
