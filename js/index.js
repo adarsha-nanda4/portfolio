03021848c759a07172dc84ac5ef6b76397c12205
Adarsh Nanda <adarshananda51@gmail.com>
2024-08-17 23:43:24 +0530

    ..

--- js/index.js
+++ js/index.js
@@ -53,3 +53,40 @@ var typed = new Typed('#codetext', {
 //     })
 // }
 
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js',
+          '/manifest.json',
+          '/icon-192x192.png',
+          '/icon-512x512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+    });
+  }
+  
